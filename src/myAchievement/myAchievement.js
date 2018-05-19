97e7d6af38c49f80ced47dbfa19569eefeaf46bb
lordlycat <lordly_cat@163.com>
2018-05-19 23:20:20 +0800

    add two page

--- src/myAchievement/myAchievement.js
+++ src/myAchievement/myAchievement.js
@@ -1,11 +1,72 @@
 import React, {
     Component
 } from 'react';
+import Animation from '../animation.js';
+import './myAchievement.css';
 
-class myAchievement extends Component {
+class UserHead extends Component {
     render() {
-        return (<div id="myAchievement">my</div>);
+        return (<div className="userHead">
+                    <div className="headImg">
+                        <img src="" alt=""/>
+                    </div>
+                    <p className="userName">00000000000</p>
+                    <p className="personalRank">个人总成绩排名：第<span>001</span>名</p>
+                    <p className="GPA">当前学习绩点：<span>A+</span></p>
+                    <div className="continue"></div>
+                    <div className="classBtn"></div>
+                </div>);
     }
 }
 
-export default myAchievement;
+class Rank extends Component {
+    render() {
+        return (<div className="rank">
+                    <div className="rankBox rankTitle">
+                        
+                    </div>
+                    <div className="rankBox plates">
+                        <span></span>
+                        <span>0/17</span>
+                        <span>第1名</span>
+                    </div>
+                    <div className="rankBox plates">
+                        <span></span>
+                        <span>0/17</span>
+                        <span>第1名</span>
+                    </div>
+                    <div className="rankBox plates">
+                        <span></span>
+                        <span>0/17</span>
+                        <span>第6666名</span>
+                    </div>
+                    <div className="rankBox plates">
+                        <span></span>
+                        <span>0/17</span>
+                        <span>第129名</span>
+                    </div>
+                    <div className="rankBox plates">
+                        <span></span>
+                        <span>0/17</span>
+                        <span>第98876名</span>
+                    </div>
+                </div>)
+    }
+}
+
+
+class MyAchievement extends Component {
+    componentDidMount() {
+        setTimeout(() => {
+            document.querySelector('#root').className = 'roots';
+        }, 300);
+    }
+    render() {
+        return (<div id="myAchievement">
+                    <UserHead />
+                    <Rank />
+                </div>);
+    }
+}
+
+export default MyAchievement;
